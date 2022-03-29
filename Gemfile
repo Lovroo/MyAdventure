fe071a3008f7177ec89575b756135bd18e0e168b
lovro <lovro.napotnik@scv.si>
2022-03-29 13:35:58 +0200

    Map upload

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ gem 'rails', '~> 6.1.4', '>= 6.1.4.1'
 gem 'pg'
 # Use Puma as the app server
 gem 'puma', '~> 5.0'
+#maps
+gem 'mapbox-gl-rails'
 #file validator
 gem 'file_validators'
 #simple forms
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+//
+//*= require mapbox-gl
+//= require_tree .
 
 Rails.start()
 Turbolinks.start()
