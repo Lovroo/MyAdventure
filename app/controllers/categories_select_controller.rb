b9365bfd4b8f333f2b93c271905a2c861b76d415
lovro <lovro.napotnik@scv.si>
2022-04-05 13:07:06 +0200

    Categories select (one working)

--- app/controllers/categories_select_controller.rb
+++ app/controllers/categories_select_controller.rb
@@ -1,18 +1,21 @@
 class CategoriesSelectController < ApplicationController
 before_action :authenticate_user!
+before_action :noAccess!
   def index
     @categories = Categories.all
   end
   def create
-    @FavCategory = FavCategory.new(cat_params)
+    @FavCategory = params[:cat_params]
+    @FavCategory.each do |c|
+      @FavCategory = FavCategory.new(c)
     @FavCategory.user_id = current_user.id
     respond_to do |format|
       if @FavCategory.save
-        format.html { redirect_to root_path, notice: 'Categories successfuly selected.' }
         format.json { render :show, status: :created, location: @FavCategory }
       else
         format.json { render json: @FavCategory.errors, status: :unprocessable_entity }
       end
+    end
     end
   end
   def FavCategory
@@ -24,5 +27,10 @@ before_action :authenticate_user!
 private
 def cat_params
   params.permit(:category_id)
+end
+def noAccess!
+  if(current_user.fav_categories.count > 1)
+    redirect_to root_path
+  end
 end
 end
