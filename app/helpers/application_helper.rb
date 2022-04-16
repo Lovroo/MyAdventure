e3d4a6b4e7b2f6ee00902ef1bc5e38967d3e5947
lovro <lovro.napotnik@scv.si>
2022-04-16 14:17:01 +0200

    CSS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,17 +2,17 @@ module ApplicationHelper
   def display_navbar_session_links
     capture do
       if user_signed_in?
-        concat link_to "Domov", root_path, class: 'w3-bar-item w3-button w3-text-white'
-        concat link_to "Moj Profil", edit_user_registration_path, class: 'w3-bar-item w3-button w3-text-white'
+        concat link_to "Home", root_path, class: 'w3-bar-item w3-button w3-text-white'
+        concat link_to "Profile", edit_user_registration_path, class: 'w3-bar-item w3-button w3-text-white'
         concat link_to "My Favorites", favorites_path, class: 'w3-bar-item w3-button w3-text-white'
         if current_user.admin == true
           concat link_to "Admin", rails_admin_path, class: 'w3-bar-item w3-button w3-text-white'
         end
-        concat link_to "Odjava", destroy_user_session_path, :method => :delete, class: 'w3-bar-item w3-button w3-text-white'
+        concat link_to "Log out", destroy_user_session_path, :method => :delete, class: 'w3-bar-item w3-button w3-text-white'
       else
-        concat link_to "Domov", root_path, class: 'w3-bar-item w3-button w3-text-white'
-        concat link_to "Prijava", new_user_session_path, class: 'w3-bar-item w3-button w3-text-white'
-        concat link_to "Registracija", new_user_registration_path, class: 'w3-bar-item w3-button w3-text-white'
+        concat link_to "Home", root_path, class: 'w3-bar-item w3-button w3-text-white'
+        concat link_to "Login", new_user_session_path, class: 'w3-bar-item w3-button w3-text-white'
+        concat link_to "Register", new_user_registration_path, class: 'w3-bar-item w3-button w3-text-white'
       end
     end
   end
