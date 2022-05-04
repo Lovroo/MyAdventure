307f56353f263978136137a71eb7323fd405d27f
lovro <lovro.napotnik@scv.si>
2022-05-04 18:21:25 +0200

    mail work!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
-    :port                 => 587,
+    :port                 => 465,
     :user_name            => "ersgamejam2022",
     :password             => "zevniksef",
     :authentication       => "plain",
