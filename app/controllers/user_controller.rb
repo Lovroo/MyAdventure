ca7db22876e9ff494f5d3c0242a26d3540dfcce5
lovro <lovro.napotnik@scv.si>
2022-05-08 19:36:21 +0200

    remove user controller

--- app/controllers/user_controller.rb
+++
@@ -1,6 +0,0 @@
-class UserController < Devise::UserController
-  def index
-    @user = User.all
-    @destinations = FavDestination.where(user_id: @user.id).where(visited: false).limit(3)
-  end
-end
