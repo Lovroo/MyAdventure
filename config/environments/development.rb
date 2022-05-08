d22792c88bb6fd38a2c8ecb3d9826fda778ef1a3
Lovro Napotnik <59780027+Lovroo@users.noreply.github.com>
2022-05-08 19:38:33 +0200

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -79,12 +79,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address              => "mail.smtp2go.com",
-    :port                 => 2525,
-    :user_name            => "MainReset",
-    :password             => "P7aBxejYSctNRIGU",
-    :authentication       => "plain",
-    :enable_starttls_auto => true
+
   }
 
 end
